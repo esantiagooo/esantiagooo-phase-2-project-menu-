a35d733e544420728d03e69b879bf62942c5fd3e
Eric <eric.yanez50@qmail.cuny.edu>
2022-06-13 08:03:15 -0400

    modified src/App.js
    src/Header.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import './App.css';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Route, Switch} from "react-router-dom"
 import NavBar from './NavBar';
 import Home from './Home';
 import About from './About';
 import OrderFood from './OrderFood';
+import Header from './Header';
 
 
 
 
 function App() {
+
+// const[foodMenu, setFoodMenu]= useState([])
+// useEffect(()=>{
+//   fetch()
+// })
   return (
     <div>
+    <Header/> 
     <NavBar/>
     <Switch>
       <Route  exact path="/about">

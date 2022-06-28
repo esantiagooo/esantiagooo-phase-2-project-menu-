0822c71bed74d8ac417c54eb6c130c840d984d61
Eric <eric.yanez50@qmail.cuny.edu>
2022-06-28 09:34:23 -0400

    modified src/ItemForm.js

--- src/ItemForm.js
+++ src/ItemForm.js
@@ -2,18 +2,25 @@ import React, { useState } from "react";
 
 function ItemForm(){
     const [name, setName] = useState("")
+    const [price, setPrice] = useState("")
     const [category, setCategory] = useState("Product")
 
     function handleSubmit(e){
         e.preventDefault()
-        console.log("name:", name)
-        console.log("category:", category)
+        const itemInfo ={
+            name: name,
+            category: category,
+            price: price
+        }
+        console.log(itemInfo)
     }
     return (
         <form className="NewItem" onSubmit={handleSubmit}>
             <label  htmlFor="items name">Item's name:</label>
             <input type="text" name ="name" value={name} onChange={(e)=>setName(e.target.value)}></input>
-            
+
+            <label  htmlFor="items price">Item's price:</label>
+            <input  type="text" name="price" value={price} onChange={(e)=>setPrice(e.target.value)} ></input>
              <label  htmlFor="items name">Category:</label>
             <select name ="category" value={category} onChange={(e)=>setCategory(e.target.value)}>
                 <option value="Produce">Add by category</option>

ed99168c94a7b010259ff79f4937659b4989f36d
Eric <eric.yanez50@qmail.cuny.edu>
2022-06-30 08:57:24 -0400

    src/ShoppingCart.js

---
+++ src/ShoppingCart.js
@@ -0,0 +1,10 @@
+// import React from "react";
+
+// function ShoppingCart(){
+//     return (
+//         <aside>
+//             <h2>Cart Items</h2>
+//         </aside>
+//     )
+// }
+// export default ShoppingCart

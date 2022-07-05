356a693fa48adfbbf0c9901f630c0a3d078b107b
Eric <eric.yanez50@qmail.cuny.edu>
2022-07-05 14:37:50 -0400

    modified src/Cart.js

--- src/Cart.js
+++ src/Cart.js
@@ -14,7 +14,7 @@ function Cart({cartItems}){
                               <h2 className="itemName">{item.name}</h2>
                               <p className="itemDescription">{item.description}</p>
                               <p className="itemPrice">{item.price}</p>
-                              {/* <button className="button" onClick={(e)=> addToCart(e)}>Add to cart</button> */}
+                              <button className="button" >Remove</button>
                           </div>
                     
                   
